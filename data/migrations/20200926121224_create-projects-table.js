e7b6dec3e61c419820f0de3db2a391c890fceb81
Chris Lien <clien@sharpencx.com>
2020-09-26 12:34:38 -0400

    created the tables and migrated the database

--- data/migrations/20200926121224_create-projects-table.js
+++ data/migrations/20200926121224_create-projects-table.js
@@ -1,8 +1,51 @@
 
 exports.up = function(knex) {
-  
+    return(
+        knex.schema
+            .createTable('projects', tbl => {
+                tbl.increments();
+                tbl.string('name', 255).notNullable();
+                tbl.string('description', 255).notNullable();
+                tbl.date('created');
+                tbl.boolean('complete', false)
+            })
+          
+            .createTable('steps', tbl => {
+                tbl.increments();
+                tbl.integer('project_id')
+                    .unsigned()
+                    .notNullable()
+                    .references('id')
+                    .inTable('projects')
+                    .onDelete('CASCADE')
+                    .onUpdate('CASCADE');
+                tbl.integer('step_number');
+                tbl.string('description');
+                tbl.string('notes');
+                tbl.boolean('complete', false);
+            })
+
+            .createTable('resources', tbl => {
+                tbl.increments();
+                tbl.string('name', 255).notNullable();
+                tbl.string ('description', 255).notNullable();
+                tbl.integer('project_id')
+                    .unsigned()
+                    .notNullable()
+                    .references('id')
+                    .inTable('projects')
+                    .onDelete('CASCADE')
+                    .onUpdate('CASCADE');
+            })
+
+    )
 };
 
 exports.down = function(knex) {
-  
+    return (
+        knex.schema
+          .dropTableIfExists('projects')
+          .dropTableIfExists('steps')
+          .dropTableIfExists('resources')
+    )  
 };
